4badc3e4ca670304dee6ab486a468cbc5f049059
minhtrifit <minhtri.fit@gmail.com>
2023-11-05 12:54:36 +0700

    add login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,13 +1,99 @@
+import {
+  Flex,
+  Stack,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
 import { useTitle } from "../hooks/useTitle";
 
 import LoginNav from "../components/LoginNav";
 
+interface FormInputs {
+  username: string;
+  password: string;
+}
+
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormInputs>();
+
   useTitle("Login");
+
+  const onSubmit = (data: FormInputs) => {
+    console.log(data);
+  };
 
   return (
     <div>
       <LoginNav />
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack spacing={8} mx={"auto"} minW={"500px"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Login With Your Account</Heading>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-8"
+              >
+                <FormControl>
+                  <FormLabel htmlFor="username">Username</FormLabel>
+                  <Input
+                    id="username"
+                    type="text"
+                    {...register("username", {
+                      required: "Username is required",
+                    })}
+                  />
+                  <Text color={"red.400"}>
+                    <ErrorMessage errors={errors} name="username" />
+                  </Text>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input
+                    id="password"
+                    type="password"
+                    {...register("password", {
+                      required: "Password is required",
+                    })}
+                  />
+                  <Text color={"red.400"}>
+                    <ErrorMessage errors={errors} name="password" />
+                  </Text>
+                </FormControl>
+                <Button mt={4} colorScheme="blue" type="submit">
+                  Login
+                </Button>
+              </form>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
     </div>
   );
 };
